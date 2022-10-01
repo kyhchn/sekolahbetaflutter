8206cd48d17bce5d9094835bfb86810962d74700
ALVIN KAMAL <alvinkn.dev@gmail.com>
2022-10-01 09:55:41 +0700

    commit

--- lib/ui/shopping_cart_page.dart
+++ lib/ui/shopping_cart_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:shopping_cart/models/product.dart';
 import 'package:shopping_cart/ui/shopping_cart_item_qty.dart';
 
